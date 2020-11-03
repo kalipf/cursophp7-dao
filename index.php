2434884174feba1ddd236717fde491f66e8ba7da
Gustavo Bonato <kalipf@hotmail.com>
2020-11-03 11:40:56 -0300

    criamos a classe usuario

--- index.php
+++ index.php
@@ -2,10 +2,17 @@
 
 require_once("config.php");
 
-$sql = new Sql();
+/*$sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM TB_USUARIOS");
 
-echo var_dump($usuarios);
+echo var_dump($usuarios);*/
+
+
+$root = new Usuario();
+
+$root->loadById(4);
+
+echo $root;
 
 ?>
