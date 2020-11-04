703a079ebba6057528ce6115b2ce8ea6634155d4
Gustavo Bonato <kalipf@hotmail.com>
2020-11-04 09:23:31 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -8,11 +8,23 @@ $usuarios = $sql->select("SELECT * FROM TB_USUARIOS");
 
 echo var_dump($usuarios);*/
 
+// carrega um usuario 
+//$root = new Usuario();
+//$root->loadById(4);
 
-$root = new Usuario();
+// carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-$root->loadById(4);
+//carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("an");
+//echo json_encode($search);
 
-echo $root;
+// carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("jose","1234567890");
+echo $usuario;
+
+//echo $root;
 
 ?>
